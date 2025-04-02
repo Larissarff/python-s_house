987adb1135703b9759b3216ed5fa7064a6344546
Larissarff <larissa.rfferreira4800@gmail.com>
2025-04-02 19:09:43 -0300

    feat: create file modification methods

--- python-house_/manipulacao_de_arquivos/gravacao_de_arquivos.py
+++ python-house_/manipulacao_de_arquivos/gravacao_de_arquivos.py
@@ -1,1 +1,17 @@
+# Abrindo o arquivo para gravação (alteração)
+arq2 = open("C:\\Users\\Pichau\\Downloads\\Python\\pythons_house\\python-s_house\\python-house_\\manipulacao_de_arquivos\\arquivos\\arquivo2.txt", "w") # w = write
 
+# como abrimos o código para gravação, ele não é habilitado para leitura
+
+
+# gravando no arquivo
+arq2.write("Eu nao sei pra onde ir, sinto um vazio por dentro \nMe escondo pra fugir desse sentimento")
+
+
+arq2.write("\nGuardei meus segredos \nRepeti meus erros mas ainda estou aqui")
+
+
+arq2 = open("C:\\Users\\Pichau\\Downloads\\Python\\pythons_house\\python-s_house\\python-house_\\manipulacao_de_arquivos\\arquivos\\arquivo2.txt", "r") # assim para visualizar
+# o conteúdo do arquivo, habilitamos o modo de leitura
+
+print(arq2.read()) 
