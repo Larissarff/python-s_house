3fe4166230374db44bde6e3a2da23907c7eb9ccb
LARISSA RODRIGUES FONTOURA FERREIRA <06011175@acad.unifeso.lan>
2025-04-03 18:20:30 -0300

    fix: dataset manipulation

--- python-house_/manipulacao_de_arquivos/gravacao_de_arquivos.py
+++ python-house_/manipulacao_de_arquivos/gravacao_de_arquivos.py
@@ -3,13 +3,10 @@ arq2 = open("C:\\Users\\Pichau\\Downloads\\Python\\pythons_house\\python-s_house
 
 # como abrimos o código para gravação, ele não é habilitado para leitura
 
-
 # gravando no arquivo
 arq2.write("Eu nao sei pra onde ir, sinto um vazio por dentro \nMe escondo pra fugir desse sentimento")
 
-
 arq2.write("\nGuardei meus segredos \nRepeti meus erros mas ainda estou aqui")
-
 
 arq2 = open("C:\\Users\\Pichau\\Downloads\\Python\\pythons_house\\python-s_house\\python-house_\\manipulacao_de_arquivos\\arquivos\\arquivo2.txt", "r") # assim para visualizar
 # o conteúdo do arquivo, habilitamos o modo de leitura
--- python-house_/manipulacao_de_arquivos/manipulando_dataset.py
+++ python-house_/manipulacao_de_arquivos/manipulando_dataset.py
@@ -1,1 +1,16 @@
+# dados retirados do site https://data.cityofchicago.org/
 
+f = open('"C:\\Users\\06011175\\larissa\\pythons_house\\python-s_house\\python-house_\\manipulacao_de_arquivos\\arquivos\\salarios.csv', 'r') # r habilitando leitura
+# lembrando que o caminho muda conforme a posição da sua pasta
+
+data = f.read() # lendo o arquivo
+
+rows = data.split('\n') # separando as linhas com base no delimitador
+
+full_data = []
+
+for row in rows:
+    split_row = row.split(",")
+    full_data.append(split_row)
+
+print(full_data)
