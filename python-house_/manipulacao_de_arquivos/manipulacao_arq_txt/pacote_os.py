d059f599f57efd316c426d75e256dc4599541fca
Larissarff <larissa.rfferreira4800@gmail.com>
2025-04-10 18:29:43 -0300

    feat: importação de arquivos com import os

---
+++ python-house_/manipulacao_de_arquivos/manipulacao_arq_txt/pacote_os.py
@@ -0,0 +1,25 @@
+# Arquivos txt são universais e podem ser lidos por praticamente qualquer dispositivo ou sistema operacional
+
+import os
+
+texto = "Ciência de Dados é uma ótima alternativa para carreira./n"
+texto = texto + "Esses profissionais costumam manipular python./n"
+texto += "E, claro, devem ser proeficientes em Data Science"
+
+"""
+Dessa forma abaixo, criamos o arquivo cientista.txt utilizando o pacote os
+"""
+arquivo = open(os.path.join('C:/Users/Pichau/Downloads/Python/pythons_house/python-s_house-1/python-house_/manipulacao_de_arquivos/manipulacao_arq_txt/cientista.txt'), "w")
+
+# método de gravar os dados no arquivo
+for palavra in texto.split(): # quando o split possui seus parênterses vazios, ele assume espaço como separação.
+    arquivo.write(palavra + ' ')
+
+arquivo.close()
+
+# abrindo arquivo para leitura
+arquivo = open('C:/Users/Pichau/Downloads/Python/pythons_house/python-s_house-1/python-house_/manipulacao_de_arquivos/manipulacao_arq_txt/cientista.txt', 'r')
+
+conteudo = arquivo.read()
+arquivo.close()
+print(conteudo)
